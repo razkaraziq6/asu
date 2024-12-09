959940314c3a619cfb71868c2000a0fa002cf7c0
razkaraziq6 <raziqrazkaraziqgantengsekali@gmail.com>
2024-12-09 11:52:05 +0700

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,24 @@
+/*
+𝗕𝘂𝘆 𝗦𝗰 𝗡𝗼 𝗘𝗻𝗰? 𝟭𝟱𝗞
+
+𝗪𝗵𝗮𝘁𝘀𝗔𝗽𝗽 : 082295620566
+𝗧𝗲𝗹𝗲𝗴𝗿𝗮𝗺 : t.me/hafizhshopp
+
+*/
+
+const settings = {
+  token: '-', // Token Bot Telegram
+  adminId: '-', // ID Tele Owner
+  urladmin: 'https://t.me/hafizhshopp',
+  pp: 'https://pomf2.lain.la/f/ws8tj94q.jpg',
+    //SERVER 1
+  domain: 'https://-', // Isi dengan domain yang digunakan
+  plta: '-', // Isi dengan nilai plta yang sesuai
+  pltc: '-', // Isi dengan nilai pltc yang sesuai
+  
+  //CREATE PANEL
+  loc: '1', // Isi dengan lokasi yang diinginkan
+  eggs: '15'
+};
+
+module.exports = settings;
---
+++ start.js
@@ -0,0 +1,12 @@
+/*
+Buy No Enc? 15K Aja
+
+WhatsApp : 082295620566
+Telegram : t.me/hafizhshopp
+
+Script Ini Murni Buatan Sendiri, Saya Hanya Sekedar Kroco Penghuni Inti Bumi.
+
+• Recode By ( Hafizh )
+*/
+
+require("./Dhan")
